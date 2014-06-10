d577490d0dd683264a3ec1b355984bd7048b1611
deadprogram <ron@hybridgroup.com>
2014-06-10 12:21:10 -0700

    Initial WIP on Crazyradio rules

--- commands/crazyflie.go
+++ commands/crazyflie.go
@@ -3,6 +3,8 @@ package commands
 import (
 	"fmt"
 	"github.com/codegangsta/cli"
+	"os"
+	"runtime"
 )
 
 func Crazyflie() cli.Command {
@@ -30,10 +32,38 @@ func Crazyflie() cli.Command {
 
 			switch c.Args().First() {
 			case "set-udev-rules":
-
-				fmt.Println("set-udev-rules here...")
+				if runtime.GOOS == "darwin" || runtime.GOOS == "linux" {
+					crazyflieSetUdevRules()
+				} else {
+					fmt.Println("No need to set-udev-rules on your OS")
+				}
 
 			}
 		},
+	}
+}
+
+func crazyflieSetUdevRules() {
+	fileExists, _ := exists("/etc/udev/rules.d/99-crazyradio.rules")
+	if !fileExists {
+		file, err := os.Create("/etc/udev/rules.d/99-crazyradio.rules")
+		if err != nil {
+			if os.IsPermission(err) {
+				fmt.Println("You do not have the required permissions. Try running this command using 'sudo'")
+			} else {
+				fmt.Println(err)
+			}
+			return
+		}
+		defer file.Close()
+
+		data, _ := Asset("support/crazyflie/crazyradio.rules")
+		file.Write(data)
+		file.Sync()
+
+		if err != nil {
+			fmt.Println("Error while copying Crazyradio udev rules")
+			return
+		}
 	}
 }
---
+++ commands/util.go
@@ -0,0 +1,108 @@
+package commands
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+)
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func downloadFromUrl(dirName string, url string) string {
+	tokens := strings.Split(url, "/")
+	fileName := tokens[len(tokens)-1]
+	fmt.Println("Downloading", url, "to", fileName)
+
+	// TODO: check file existence first with io.IsExist
+	output, err := os.Create(dirName + "/" + fileName)
+	if err != nil {
+		fmt.Println("Error while creating", fileName, "-", err)
+		return ""
+	}
+	defer output.Close()
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return ""
+	}
+	defer response.Body.Close()
+
+	n, err := io.Copy(output, response.Body)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return ""
+	}
+
+	fmt.Println(n, "bytes downloaded.")
+	return fileName
+}
+
+func gortDirName() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return usr.HomeDir + "/" + "gort"
+}
+
+func createGortDirectory() (string, error) {
+	dirName := gortDirName()
+	fileExists, err := exists(dirName)
+	if fileExists {
+		fmt.Println("Gort lives")
+	} else {
+		os.Mkdir(dirName, 0777)
+	}
+	return dirName, err
+}
+
+func Unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+
+		path := filepath.Join(dest, f.Name)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			f, err := os.OpenFile(
+				path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
