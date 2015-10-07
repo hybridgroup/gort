29ba3a8e8a81427b326ddbd4326063de2e09defd
deadprogram <ron@hybridgroup.com>
2015-10-07 10:37:48 -0700

    Bluetooth connect command updated for BlueZ 5.x

--- commands/bluetooth.go
+++ commands/bluetooth.go
@@ -16,17 +16,16 @@ func Bluetooth() cli.Command {
 		Usage: "Pair, unpair & connect to bluetooth devices.",
 		Action: func(c *cli.Context) {
 			valid := false
-			for _, s := range []string{"pair", "unpair", "connect"} {
+			for _, s := range []string{"connect", "disconnect"} {
 				if s == c.Args().First() {
 					valid = true
 				}
 			}
 			usage := func() {
-				fmt.Println("Invalid/no subcommand supplied.\n")
+				fmt.Println("Invalid/no subcommand supplied.")
 				fmt.Println("Usage:")
-				fmt.Println("gort bluetooth pair <address> [hciX]")
-				fmt.Println("gort bluetooth unpair <address> [hciX]")
-				fmt.Println("gort bluetooth connect <dev> [hciX]\n")
+				fmt.Println("gort bluetooth connect <address> [hciX]")
+				fmt.Println("gort bluetooth disconnect <address> [hciX]")
 			}
 
 			if runtime.GOOS == "darwin" {
@@ -44,31 +43,23 @@ func Bluetooth() cli.Command {
 			}
 
 			hci := "hci0"
-			if len(c.Args()) <= 3 {
+			if len(c.Args()) >= 3 {
 				hci = c.Args()[2]
 			}
 
 			switch runtime.GOOS {
 			case "linux":
 				switch c.Args().First() {
-				case "pair":
-					cmd := exec.Command("bluez-simple-agent", hci, c.Args()[1])
-					cmd.Stdout = os.Stdout
-					cmd.Stderr = os.Stderr
-					if err := cmd.Run(); err != nil {
-						log.Fatal(err)
-					}
-
-				case "unpair":
-					cmd := exec.Command("bluez-simple-agent", hci, c.Args()[1], "remove")
-					cmd.Stdout = os.Stdout
-					cmd.Stderr = os.Stderr
-					if err := cmd.Run(); err != nil {
-						log.Fatal(err)
-					}
-
 				case "connect":
-					cmd := exec.Command("bluez-test-serial", "-i", hci, c.Args()[1])
+					cmd := exec.Command("bt-device", "-a", hci, "-c", c.Args()[1])
+					cmd.Stdout = os.Stdout
+					cmd.Stderr = os.Stderr
+					if err := cmd.Run(); err != nil {
+						log.Fatal(err)
+					}
+
+				case "disconnect":
+					cmd := exec.Command("bt-device", "-a", hci, "-d", c.Args()[1])
 					cmd.Stdout = os.Stdout
 					cmd.Stderr = os.Stderr
 					if err := cmd.Run(); err != nil {
