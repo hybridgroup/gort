03069c159cb63a8b3b2cee4159860e14d1b4d17e
deadprogram <ron@hybridgroup.com>
2015-10-23 21:06:55 -0700

    Removing DD again

--- gort.go
+++ gort.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"os"
-
 	"github.com/codegangsta/cli"
 	"github.com/hybridgroup/gort/commands"
+	"os"
 )
 
 func main() {
@@ -20,7 +19,6 @@ func main() {
 		commands.Digispark(),
 		commands.Crazyflie(),
 		commands.Klaatu(),
-		commands.DroneDrop(),
 	}
 	app.Run(os.Args)
 }
