41dc46c7a13c3f219c77e719a1ece1ac22e52352
deadprogram <ron@hybridgroup.com>
2016-05-01 15:39:37 -0700

    Add debian control file for correct build
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- commands/bindata.go
+++ commands/bindata.go
@@ -19,11 +19,11 @@ import (
 	"compress/gzip"
 	"fmt"
 	"io"
-	"strings"
-	"os"
-	"time"
 	"io/ioutil"
+	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
 func bindataRead(data []byte, name string) ([]byte, error) {
@@ -52,9 +52,9 @@ type asset struct {
 }
 
 type bindataFileInfo struct {
-	name string
-	size int64
-	mode os.FileMode
+	name    string
+	size    int64
+	mode    os.FileMode
 	modTime time.Time
 }
 
@@ -93,7 +93,7 @@ func supportArduinoFirmataCppHex() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "support/arduino/firmata.cpp.hex", size: 32225, mode: os.FileMode(436), modTime: time.Unix(1398150306, 0)}
-	a := &asset{bytes: bytes, info:  info}
+	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
 
@@ -113,7 +113,7 @@ func supportArduinoRapiroCppHex() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "support/arduino/rapiro.cpp.hex", size: 29955, mode: os.FileMode(436), modTime: time.Unix(1398150306, 0)}
-	a := &asset{bytes: bytes, info:  info}
+	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
 
@@ -133,7 +133,7 @@ func supportCrazyflieCrazyradioRules() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "support/crazyflie/crazyradio.rules", size: 98, mode: os.FileMode(436), modTime: time.Unix(1402597516, 0)}
-	a := &asset{bytes: bytes, info:  info}
+	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
 
@@ -153,7 +153,7 @@ func supportDigisparkMicronucleusRules() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "support/digispark/micronucleus.rules", size: 1002, mode: os.FileMode(436), modTime: time.Unix(1402597516, 0)}
-	a := &asset{bytes: bytes, info:  info}
+	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
 
@@ -173,7 +173,7 @@ func supportDrone_dropRcsDronedrop() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "support/drone_drop/rcS.dronedrop", size: 3482, mode: os.FileMode(436), modTime: time.Unix(1445662657, 0)}
-	a := &asset{bytes: bytes, info:  info}
+	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
 
@@ -193,7 +193,7 @@ func supportDrone_dropRcsStock() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "support/drone_drop/rcS.stock", size: 3442, mode: os.FileMode(436), modTime: time.Unix(1445662657, 0)}
-	a := &asset{bytes: bytes, info:  info}
+	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
 
@@ -213,7 +213,7 @@ func supportDrone_dropUsbIdsDronedrop() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "support/drone_drop/usb.ids.dronedrop", size: 172047, mode: os.FileMode(436), modTime: time.Unix(1445662657, 0)}
-	a := &asset{bytes: bytes, info:  info}
+	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
 
@@ -233,7 +233,7 @@ func supportDrone_dropUsbIdsStock() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "support/drone_drop/usb.ids.stock", size: 171980, mode: os.FileMode(436), modTime: time.Unix(1445662657, 0)}
-	a := &asset{bytes: bytes, info:  info}
+	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
 
@@ -253,7 +253,7 @@ func supportParticleDefaultCpp() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "support/particle/default.cpp", size: 2171, mode: os.FileMode(436), modTime: time.Unix(1445662502, 0)}
-	a := &asset{bytes: bytes, info:  info}
+	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
 
@@ -273,7 +273,7 @@ func supportParticleVoodoosparkCpp() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "support/particle/voodoospark.cpp", size: 20285, mode: os.FileMode(436), modTime: time.Unix(1445662502, 0)}
-	a := &asset{bytes: bytes, info:  info}
+	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
 
@@ -296,7 +296,7 @@ func Asset(name string) ([]byte, error) {
 // It simplifies safe initialization of global variables.
 func MustAsset(name string) []byte {
 	a, err := Asset(name)
-	if (err != nil) {
+	if err != nil {
 		panic("asset: Asset(" + name + "): " + err.Error())
 	}
 
@@ -377,88 +377,78 @@ func AssetDir(name string) ([]string, error) {
 }
 
 type bintree struct {
-	Func func() (*asset, error)
+	Func     func() (*asset, error)
 	Children map[string]*bintree
 }
 var _bintree = &bintree{nil, map[string]*bintree{
 	"support": &bintree{nil, map[string]*bintree{
 		"arduino": &bintree{nil, map[string]*bintree{
-			"firmata.cpp.hex": &bintree{supportArduinoFirmataCppHex, map[string]*bintree{
-			}},
-			"rapiro.cpp.hex": &bintree{supportArduinoRapiroCppHex, map[string]*bintree{
-			}},
+			"firmata.cpp.hex": &bintree{supportArduinoFirmataCppHex, map[string]*bintree{}},
+			"rapiro.cpp.hex": &bintree{supportArduinoRapiroCppHex, map[string]*bintree{}},
 		}},
 		"crazyflie": &bintree{nil, map[string]*bintree{
-			"crazyradio.rules": &bintree{supportCrazyflieCrazyradioRules, map[string]*bintree{
-			}},
+			"crazyradio.rules": &bintree{supportCrazyflieCrazyradioRules, map[string]*bintree{}},
 		}},
 		"digispark": &bintree{nil, map[string]*bintree{
-			"micronucleus.rules": &bintree{supportDigisparkMicronucleusRules, map[string]*bintree{
-			}},
+			"micronucleus.rules": &bintree{supportDigisparkMicronucleusRules, map[string]*bintree{}},
 		}},
 		"drone_drop": &bintree{nil, map[string]*bintree{
-			"rcS.dronedrop": &bintree{supportDrone_dropRcsDronedrop, map[string]*bintree{
-			}},
-			"rcS.stock": &bintree{supportDrone_dropRcsStock, map[string]*bintree{
-			}},
-			"usb.ids.dronedrop": &bintree{supportDrone_dropUsbIdsDronedrop, map[string]*bintree{
-			}},
-			"usb.ids.stock": &bintree{supportDrone_dropUsbIdsStock, map[string]*bintree{
-			}},
+			"rcS.dronedrop": &bintree{supportDrone_dropRcsDronedrop, map[string]*bintree{}},
+			"rcS.stock": &bintree{supportDrone_dropRcsStock, map[string]*bintree{}},
+			"usb.ids.dronedrop": &bintree{supportDrone_dropUsbIdsDronedrop, map[string]*bintree{}},
+			"usb.ids.stock": &bintree{supportDrone_dropUsbIdsStock, map[string]*bintree{}},
 		}},
 		"particle": &bintree{nil, map[string]*bintree{
-			"default.cpp": &bintree{supportParticleDefaultCpp, map[string]*bintree{
-			}},
-			"voodoospark.cpp": &bintree{supportParticleVoodoosparkCpp, map[string]*bintree{
-			}},
+			"default.cpp": &bintree{supportParticleDefaultCpp, map[string]*bintree{}},
+			"voodoospark.cpp": &bintree{supportParticleVoodoosparkCpp, map[string]*bintree{}},
 		}},
 	}},
 }}
 
 // RestoreAsset restores an asset under the given directory
 func RestoreAsset(dir, name string) error {
-        data, err := Asset(name)
-        if err != nil {
-                return err
-        }
-        info, err := AssetInfo(name)
-        if err != nil {
-                return err
-        }
-        err = os.MkdirAll(_filePath(dir, filepath.Dir(name)), os.FileMode(0755))
-        if err != nil {
-                return err
-        }
-        err = ioutil.WriteFile(_filePath(dir, name), data, info.Mode())
-        if err != nil {
-                return err
-        }
-        err = os.Chtimes(_filePath(dir, name), info.ModTime(), info.ModTime())
-        if err != nil {
-                return err
-        }
-        return nil
+	data, err := Asset(name)
+	if err != nil {
+		return err
+	}
+	info, err := AssetInfo(name)
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(_filePath(dir, filepath.Dir(name)), os.FileMode(0755))
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(_filePath(dir, name), data, info.Mode())
+	if err != nil {
+		return err
+	}
+	err = os.Chtimes(_filePath(dir, name), info.ModTime(), info.ModTime())
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // RestoreAssets restores an asset under the given directory recursively
 func RestoreAssets(dir, name string) error {
-        children, err := AssetDir(name)
-        // File
-        if err != nil {
-                return RestoreAsset(dir, name)
-        }
-        // Dir
-        for _, child := range children {
-                err = RestoreAssets(dir, filepath.Join(name, child))
-                if err != nil {
-                        return err
-                }
-        }
-        return nil
+	children, err := AssetDir(name)
+	// File
+	if err != nil {
+		return RestoreAsset(dir, name)
+	}
+	// Dir
+	for _, child := range children {
+		err = RestoreAssets(dir, filepath.Join(name, child))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func _filePath(dir, name string) string {
-        cannonicalName := strings.Replace(name, "\\", "/", -1)
-        return filepath.Join(append([]string{dir}, strings.Split(cannonicalName, "/")...)...)
+	cannonicalName := strings.Replace(name, "\\", "/", -1)
+	return filepath.Join(append([]string{dir}, strings.Split(cannonicalName, "/")...)...)
 }
 
